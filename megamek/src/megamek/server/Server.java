12911ef0fe55fee0729bfea9cf2842df0b6195c3
AdamSwice <65133423+AdamSwice@users.noreply.github.com>
2022-04-05 21:36:57 -0400

    validatePort moved to validationService (deprecated)

--- megamek/src/megamek/server/Server.java
+++ megamek/src/megamek/server/Server.java
@@ -305,24 +305,6 @@ public class Server implements Runnable {
 
     };
 
-    /**
-     *
-     * @param port if 0 or less, will return default, if illegal number, throws ParseException
-     * @return valid port number
-     */
-    public static int validatePort(int port) throws AbstractCommandLineParser.ParseException {
-        if (port <= 0) {
-            return MMConstants.DEFAULT_PORT;
-        }
-
-        if ((port < MMConstants.MIN_PORT) || (port > MMConstants.MAX_PORT)) {
-            String msg = String.format("Port number %d outside allowed range %d-%d", port, MMConstants.MIN_PORT, MMConstants.MAX_PORT);
-            LogManager.getLogger().error(msg);
-            throw new AbstractCommandLineParser.ParseException(msg);
-
-        }
-        return port;
-    }
 
     /**
      * Used to ensure only one thread at a time is accessing this particular
